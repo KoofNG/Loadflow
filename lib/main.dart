c2680ad9edfc8fee9a9dff9d5dd2d6a8d9566b24
KoofNG <isrealakpan296@gmail.com>
2019-03-16 03:40:24 +0100

    Changed gradle file contents

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,12 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Power Load Flow',
       theme: new ThemeData(
         primaryColor: Colors.amberAccent,
-        primaryIconTheme: IconThemeData(
-          color: Colors.white
-        )
+        primaryIconTheme: IconThemeData(color: Colors.white),
       ),
       home: new HomePage(),
     );
